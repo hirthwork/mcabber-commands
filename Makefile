6746c78b5d8e547c076eea79d736520a3b93e57d
Dmitry Potapov <potapov.d@gmail.com>
2014-11-02 23:12:53 +0300

    various bugfixes
    "quotation" marks allowed in messages
    fix segfault on invalid regex
    suppress output from spawned commands

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+CC := gcc
+CFLAGS := -pedantic -Wall -Wextra
+
 all:
-	gcc commands.c $(CFLAGS) -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include/ -I/usr/include/loudmouth-1.0/ -std=c99 -lglib-2.0 -shared -DMODULES_ENABLE -o libcommands.so -fPIC
+	$(CC) commands.c $(CFLAGS) -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include/ -I/usr/include/loudmouth-1.0/ -std=c99 -lglib-2.0 -shared -DMODULES_ENABLE -o libcommands.so -fPIC
 
 install: all
 	install -D libcommands.so "$(DESTDIR)"/libcommands.so
--- commands.c
+++ commands.c
@@ -17,6 +17,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef MAX_ARGS
+#define MAX_ARGS 32
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 
@@ -38,13 +42,68 @@ static GSList* custom_commands;
 static guint msg_in_hid;
 static guint custom_commands_compl_categ;
 
+static gchar* print_command(char* prev, char* curr, char* args) {
+    gchar* tmp = g_strndup(prev, curr - prev);
+    gchar* result = g_strdup_printf(tmp, args);
+    g_free(tmp);
+    return result;
+}
+
 static void handle_command(char* args, gpointer data)
 {
+    GError* err = 0;
     struct custom_command* command = (struct custom_command*)data;
-    gchar* cmd = g_strdup_printf(command->command, args);
-    scr_log_print(LPRINT_LOGNORM, "executing: '%s'", cmd);
-    g_spawn_command_line_async(cmd, NULL);
-    g_free(cmd);
+    gchar* argv[MAX_ARGS + 2] = {0};
+    ssize_t argc = 0;
+    char* prev = command->command;
+    char* ptr = command->command;
+    while (argc < MAX_ARGS && *ptr) {
+        if (*ptr == '"') {
+            prev = ++ptr;
+            while (*ptr && *ptr != '"') {
+                ++ptr;
+            }
+            argv[argc++] = print_command(prev, ptr, args);
+            if (*ptr == '"') {
+                if (*++ptr) {
+                    ++ptr;
+                }
+            }
+            prev = ptr;
+        } else {
+            while (*ptr && *ptr != ' ') {
+                ++ptr;
+            }
+            argv[argc++] = print_command(prev, ptr, args);
+            if (*ptr) {
+                ++ptr;
+            }
+            prev = ptr;
+        }
+    }
+    g_spawn_async(
+        0,
+        argv,
+        0,
+        G_SPAWN_SEARCH_PATH
+        | G_SPAWN_STDOUT_TO_DEV_NULL
+        | G_SPAWN_STDERR_TO_DEV_NULL,
+        0,
+        0,
+        0,
+        &err);
+    if (err) {
+        scr_log_print(
+            LPRINT_NORMAL,
+            "Failed to execute command '%s' with arg '%s': %s",
+            command->command,
+            args,
+            err->message);
+        g_error_free(err);
+    }
+    while (--argc >= 0) {
+        g_free(argv[argc]);
+    }
 }
 
 static void delete_custom_command(gpointer data)
@@ -97,7 +156,7 @@ static void del_custom_command(char* args)
 
 static void add_custom_command(char* args)
 {
-    struct custom_command* command;
+    GError* err = 0;
     char* name = next_token(args);
     char* buddy_regex = next_token(name ? (name + strlen(name) + 1) : 0);
     char* input_regex =
@@ -110,11 +169,27 @@ static void add_custom_command(char* args)
         return;
     }
     del_custom_command(name);
-    command = g_malloc(sizeof(struct custom_command));
+    struct custom_command* command = g_malloc0(sizeof(struct custom_command));
     command->name = g_strdup(name);
     command->buddy_regex = g_regex_new(buddy_regex,
-        G_REGEX_OPTIMIZE|G_REGEX_ANCHORED, G_REGEX_MATCH_ANCHORED, NULL);
-    command->input_regex = g_regex_new(input_regex, G_REGEX_OPTIMIZE, 0, NULL);
+        G_REGEX_OPTIMIZE|G_REGEX_ANCHORED, G_REGEX_MATCH_ANCHORED, &err);
+    if (err) {
+        scr_log_print(
+            LPRINT_NORMAL,
+            "Failed to compile buddy name pattern '%s': %s",
+            buddy_regex,
+            err->message);
+        goto error;
+    }
+    command->input_regex = g_regex_new(input_regex, G_REGEX_OPTIMIZE, 0, &err);
+    if (err) {
+        scr_log_print(
+            LPRINT_NORMAL,
+            "Failed to compile input pattern '%s': %s",
+            input_regex,
+            err->message);
+        goto error;
+    }
     command->command = g_strdup(input_regex + strlen(input_regex) + 1);
     command->completion_id = compl_new_category(0x30);
     // TODO: pass some meaningful help string
@@ -122,6 +197,14 @@ static void add_custom_command(char* args)
         (void (*)(char*))handle_command, command);
     custom_commands = g_slist_prepend(custom_commands, command);
     compl_add_category_word(custom_commands_compl_categ, name);
+    return;
+error:
+    g_error_free(err);
+    g_free(command->name);
+    if (command->buddy_regex) {
+        g_regex_unref(command->buddy_regex);
+    }
+    g_free(command);
 }
 
 static void list_custom_commands(char* args)
@@ -130,6 +213,7 @@ static void list_custom_commands(char* args)
     struct custom_command* command;
     gchar *newstr;
     gchar *str = "";
+    (void) args;
 
     for (cmd = custom_commands; cmd; cmd = g_slist_next(cmd)) {
         command = (struct custom_command*)cmd->data;
@@ -151,11 +235,9 @@ static void list_custom_commands(char* args)
 static void show_custom_command(char* args)
 {
     guint dynlist;
-    GSList* cmd;
-    struct custom_command* command;
 
-    for (cmd = custom_commands; cmd; cmd = g_slist_next(cmd)) {
-        command = (struct custom_command*)cmd->data;
+    for (GSList* cmd = custom_commands; cmd; cmd = g_slist_next(cmd)) {
+        struct custom_command* command = (struct custom_command*)cmd->data;
         if (!strcmp(command->name, args)) {
             scr_log_print(LPRINT_NORMAL, "Custom command '%s' greps messages "
                 "from user(s) '%s' for strings matching '%s' and executes "
@@ -180,6 +262,8 @@ static guint parse_message(const gchar *hookname, hk_arg_t *args,
     gchar* word;
     GMatchInfo* match_info;
     struct custom_command* command;
+    (void) hookname;
+    (void) userdata;
 
     for (; args->name; ++args) {
         if (!bjid && !g_strcmp0(args->name, "jid")) {
